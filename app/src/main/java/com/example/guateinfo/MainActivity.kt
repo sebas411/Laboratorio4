54a31c601d4c2a589e44fc0c2cf9c0eeefa3e741
sebas411 <59925024+sebas411@users.noreply.github.com>
2020-02-02 23:39:49 -0600

    hi

--- app/src/main/java/com/example/guateinfo/MainActivity.kt
+++ app/src/main/java/com/example/guateinfo/MainActivity.kt
@@ -2,11 +2,42 @@ package com.example.guateinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var editNombre: EditText
+    lateinit var textNombre: TextView
+    lateinit var showNombre: TextView
+    private var set=true
+    private var texto=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editNombre=findViewById(R.id.editText2)
+        textNombre=findViewById(R.id.textView)
+        showNombre=findViewById(R.id.textView2)
+        showNombre.visibility = View.INVISIBLE
+        val starButton:ImageButton=findViewById(R.id.starButton)
+        starButton.setOnClickListener{
+            setText()
+        }
+
+    }
+    private fun setText() {
+        if (set) {
+            showNombre.setText(editNombre.getText())
+            editNombre.visibility = View.INVISIBLE
+            textNombre.visibility = View.INVISIBLE
+            showNombre.visibility = View.VISIBLE
+        }else{
+            editNombre.visibility = View.VISIBLE
+            textNombre.visibility = View.VISIBLE
+            showNombre.visibility = View.INVISIBLE
+        }
+        set=!set
     }
 }
